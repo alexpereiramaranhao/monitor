5616606880b5dafc64f6655a79e7abf22e73416d
Alex Pereira Maranhão <alexpereira365@gmail.com>
2019-01-31 13:27:32 -0200

    Adiciona funcionalidades para funções

--- monitor.go
+++ monitor.go
@@ -2,37 +2,62 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-
-	var opcao int
-
-	montarMenu()
-
-	fmt.Scan(&opcao)
-
-	if opcao == 1 {
-		fmt.Println("Monitoramento")
-	} else if opcao == 2 {
-		fmt.Println("Logs")
-	} else if opcao == 0 {
-		fmt.Println("Saindo...")
-	} else {
-		fmt.Println("Opção inválida:")
-		montarMenu()
-	}
-
-}
-
-func montarMenu() {
 	versao := 1.1
 
 	fmt.Println("Monitor sistema, versão", versao)
+
+	for {
+		montarMenu()
+
+		opcao := lerComando()
+
+		switch opcao {
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Logs")
+		default:
+			fmt.Println("Opção inválida:")
+			os.Exit(-1)
+		}
+	}
+}
+
+func lerComando() int {
+	var opcao int
+
+	fmt.Scan(&opcao)
+
+	return opcao
+}
+
+func montarMenu() {
 
 	fmt.Println("Menu")
 
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+}
+
+func iniciarMonitoramento() {
+	site := "https://random-status-code.herokuapp.com/"
+
+	response, _ := http.Get(site)
+
+	statusCode := response.StatusCode
+
+	if statusCode == 200 {
+		fmt.Println("Status ok:", statusCode)
+	} else {
+		fmt.Println("Erro:", statusCode)
+	}
 }
